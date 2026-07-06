9a2b54c45994d266f1bd083302fae0a4a322b608
Mohamed Mohsen <mohsen.contact@proton.me>
2026-07-06 14:34:44 +0300

    Add uninstall script to remove terminal shortcuts

---
+++ install/uninstall.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Removing Terminal Shortcuts..."
+
+rm -f ~/.local/bin/update
+rm -f ~/.local/bin/cleaner
+rm -f ~/.local/bin/doctor
+rm -f ~/.local/bin/repair
+
+echo "Done."
